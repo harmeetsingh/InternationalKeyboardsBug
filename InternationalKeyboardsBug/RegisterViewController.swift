e7df8d8e7c014934d35450977b8cb70ed76ac3a6
Harmeet Singh <harmeetsingh11@hotmail.com>
2019-06-05 15:32:12 +0200

    add source code

---
+++ InternationalKeyboardsBug/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  InternationalKeyboardsBug
+//
+//  Created by Harmeet Singh on 04/06/2019.
+//  Copyright © 2019 Harmeet Singh. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
